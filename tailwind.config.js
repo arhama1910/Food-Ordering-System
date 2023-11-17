ef81924754711caba85c4094d71af5102939ead6
Arhama Ansari <95290641+arhama1910@users.noreply.github.com>
2023-11-17 21:34:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      width:{
+        190: "190px",
+      },
+      height:{
+        80: "80px",
+        150: "150px",
+        225: "225px",
+        300: "300px",
+        340: "340px",
+        370: "370px",
+        420: "420px",
+        510: "510px",
+        600: "600px",
+        650: "650px",
+        685: "685px",
+        800: "800px",
+        "90vh": "90vh",
+      },
+      minWidth: {
+        210: "210px",
+        350: "350px",
+        620: "620px",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2x1": "1536px",
+      },
+      colors:{
+        headingColor: "#2e2e2e",
+        textColor: "#515151",
+        cartNumBg: "#dc2626",
+        primary: "#f5f3f3",
+        cardOverlay: "rgba(256,256,256,0.4)",
+        lighttextGray: "#9ca0ab",
+        card: "rgba(256,256,256,0.8)",
+        cartBg: "#282a2c",
+        cartItem: "#2e3033",
+        cartTotal: "#343739",
+      }
+    },
+  },
+  
+  plugins:  [
+    require('@tailwindcss/forms'),
+  ],
+}
